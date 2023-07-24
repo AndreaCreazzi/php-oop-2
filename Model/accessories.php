6584f7b91d8b3f1e7b290ed9b237ebd924170d19
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-24 17:00:14 +0200

    fix accessories.php

--- Model/accessories.php
+++ Model/accessories.php
@@ -3,14 +3,12 @@ require_once __DIR__ . '/products.php';
 
 class Accessories extends Products
 {
-    public $categories;
     public $material;
     public $dimension;
 
-    public function __construct($title, $price, $type, $image, $categories, $material, $dimension)
+    public function __construct($title, $price, $type, $image, $material, $dimension)
     {
         parent::__construct($title, $price, $type, $image);
-        $this->categories = $categories;
         $this->material = $material;
         $this->dimension = $dimension;
     }
